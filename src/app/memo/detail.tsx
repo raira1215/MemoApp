43d97433b1404699d42dfeefd9716d77bbddf4a3
akinari <akinari1215335@gmail.com>
2023-12-10 19:07:19 +0900

    Link Create memodetail to MemoEdit

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -3,6 +3,11 @@ import { View, Text, ScrollView, StyleSheet } from 'react-native'
 import Header from '../../components/Header'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
+import { router } from 'expo-router'
+
+const handlePress = (): void => {
+  router.push('/memo/edit')
+}
 
 const Detail = (): JSX.Element => {
   return (
@@ -21,7 +26,7 @@ const Detail = (): JSX.Element => {
         </ScrollView>
       </View>
 
-      <CircleButton style={{ top: 145, bottom: 'auto' }}>
+      <CircleButton onPress={handlePress} style={{ top: 145, bottom: 'auto' }}>
         <Icon name='pencil' size={40} color='#ffffff' />
       </CircleButton>
     </View>
