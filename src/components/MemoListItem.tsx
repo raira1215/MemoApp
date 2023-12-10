a0a7996868c10dfca850df0f50882b0ffb52909d
akinari <akinari1215335@gmail.com>
2023-12-10 18:57:38 +0900

    Link Create MemoList to MemoDetail

--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-
 import Icon from './icon'
+import { Link } from 'expo-router'
+
 const memoListItem = (): JSX.Element => {
   return (
-    <View style={styles.memoListItem}>
+    <Link href={'/memo/detail'} asChild>
+    <TouchableOpacity style={styles.memoListItem}>
       <View>
         <Text style={styles.memoListItemTitle}>買い物リスト</Text>
         <Text style={styles.memoListItemDate}>2023年12月5日 10:00</Text>
@@ -11,7 +13,8 @@ const memoListItem = (): JSX.Element => {
       <TouchableOpacity>
         <Icon name='delete' size={40} color='#BEBEBE' />
       </TouchableOpacity>
-    </View>
+      </TouchableOpacity>
+    </Link>
   )
 }
 
