fb42e5dcc5f23e3c21b335072220605d88fa454e
akinari <akinari1215335@gmail.com>
2023-12-11 20:58:38 +0900

    Create Database user

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -4,19 +4,39 @@ import {
 
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
+import { collection, addDoc, Timestamp } from 'firebase/firestore'
+import { useState } from 'react'
+import { db, auth } from '../../config'
 import { router } from 'expo-router'
 
-const handlePress = (): void => {
-  router.back()
+const handlePress = (bodyText: string): void => {
+  if (auth.currentUser === null) { return }
+  const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+  addDoc(ref, {
+    bodyText,
+    updatedAt: Timestamp.fromDate(new Date())
+  })
+    .then((docRef) => {
+      console.log('succese', docRef.id)
+      router.back()
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 }
 
 const Create = (): JSX.Element => {
+  const [bodyText, setBodyText] = useState('')
   return (
     <KeyboardAvoidingView behavior='height' style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput multiline style={styles.input} />
+        <TextInput multiline
+          style={styles.input}
+          value={bodyText}
+          onChangeText={(text) => { setBodyText(text) } }
+        />
       </View>
-      <CircleButton onPress={handlePress}>
+      <CircleButton onPress={() => { handlePress(bodyText) }}>
         <Icon name='check' size={40} color='#ffffff' />
       </CircleButton>
     </KeyboardAvoidingView>
