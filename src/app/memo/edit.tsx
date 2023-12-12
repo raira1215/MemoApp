d5050d0ece589029fefcb2fa434236f6e458c553
akinari <akinari1215335@gmail.com>
2023-12-12 22:17:26 +0900

    Create KeyboardAvoidingView

--- src/app/memo/edit.tsx
+++ src/app/memo/edit.tsx
@@ -1,10 +1,11 @@
 import {
-  View, TextInput, StyleSheet, KeyboardAvoidingView, Alert
+  View, TextInput, StyleSheet, Alert
 } from 'react-native'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
 import { router, useLocalSearchParams } from 'expo-router'
 import { useState, useEffect } from 'react'
+import KeyboardAvoidingView from '../../components/KeyboardAvoidingView'
 import { doc, getDoc, setDoc, Timestamp } from 'firebase/firestore'
 import { auth, db } from '../../config'
 
@@ -41,12 +42,13 @@ const Edit = (): JSX.Element => {
   }, [])
   console.log('edit', id)
   return (
-    <KeyboardAvoidingView behavior='height' style={styles.container}>
+    <KeyboardAvoidingView style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput multiline
           style={styles.input}
           value={bodyText}
-        onChangeText={(text) => { setBodyText(text) }}/>
+          onChangeText={(text) => { setBodyText(text) }}
+          autoFocus/>
       </View>
       <CircleButton onPress={() => { handlePress(id, bodyText) }}>
         <Icon name='check' size={40} color='#ffffff' />
